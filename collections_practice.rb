8768aa65f83e3c66f70b87eb4c24c9065a6d764c
JJKOrlino <jjkorlino80@yahoo.com>
2020-11-16 18:43:54 -0600

    commit tree 6cccd522d4beaaece54a4ab3560d491b63396f5b

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
+#
+#
 
+def sort_array_asc(array)
+    array.sort
+end
+
+def sort_array_desc(array)
+    array.sort do |front , back|
+        back <=> front
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |front , back|
+        front.length <=> back.length
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2], = array[2], array[1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |name|
+        name[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.find_all do |word|
+        word[0] == "a"
+    end
+end
+
+def sum_array(array)
+    sum = 0
+    array.each do |number|
+        sum+=number
+    end
+    sum
+end
+
+def add_s(array)
+    array.collect do |word|
+        if array[1] == word
+            word
+        else
+            word + "s"
+        end
+    end
+end
